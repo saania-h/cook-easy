120fb51cf62e2b99fb137ddf6a773152d44204e9
saania <saaniahosany@gmail.com>
2025-08-03 13:54:48 +0400

    commit

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
-    $('.icon').on('click', function(){
-        $('nav ul').toggleClass('showing');
-
-    });
-    
-    $('.icon').on('mouseleave', function(){
-        $('nav ul').toggleClass('showing');
-    });
+ $('.icon').on('click', function(){
+ $('nav ul').toggleClass('showing');
+ });
+ $('nav').on('mouseleave', function(){
+ $('nav ul').toggleClass('showing');
+ });
 });
